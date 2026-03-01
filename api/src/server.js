5f69c2355fd305d13b4bf1d37e6828e03a98bb19
Yusufiabichi <yusufiabichi@yahoo.com>
2026-03-01 10:25:29 +0100

    Ready for Production

--- api/src/server.js
+++ api/src/server.js
@@ -1,10 +1,12 @@
+
 import dotenv from "dotenv";
 import app from "./app.js";
 import { testDbConnection } from "./config/db.js";
+const path = require('path');
 
 dotenv.config();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   const isDbConnected = await testDbConnection();
@@ -17,5 +19,14 @@ const startServer = async () => {
     console.log(`[Server] Listening on http://localhost:${PORT}`);
   });
 };
+
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'client/out')));
+
+// React route fallback
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/out', 'index.html'));
+});
 
 startServer();
